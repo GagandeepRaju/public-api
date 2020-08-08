622a31b857d459cdccbcc0fdda73460f5fca15e6
Gagandeep <raju.gagandeep@gmail.com>
2020-08-08 14:17:10 -0400

    clean up

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ xhr.onreadystatechange = function () {
     // Typical action to be performed when the document is ready:
     const response = JSON.parse(xhr.response);
     galleryCard(response.results);
-    console.log(response.results);
   }
 };
 xhr.open("GET", url);
